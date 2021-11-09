bbf42e020d4ccd4ceb580ffb89ad36136e8a0521
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-11-09 15:17:53 +0200

    Add Array of objects to populate scoreboard list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './style.css';
+const list = document.getElementById('scorelist');
+
+const myList = [
+  {
+    name: 'Munsa Mibenge',
+    score: 150,
+  },
+  {
+    name: 'Howard Huges',
+    score: 140,
+  },
+  {
+    name: 'Napolean Hill',
+    score: 130,
+  },
+  {
+    name: 'Shannon Biggs',
+    score: 129,
+  },
+  {
+    name: 'Crystal Summer',
+    score: 125,
+  },
+  {
+    name: 'John Lenon',
+    score: 115,
+  },
+  {
+    name: 'Chris Pratt',
+    score: 110,
+  },
+  {
+    name: 'The Rock',
+    score: 108,
+  },
+  {
+    name: 'Mike Myers',
+    score: 103,
+  },
+  {
+    name: 'Eddie Murphy',
+    score: 120,
+  }
+];
+
+myList.forEach((populate)=>{
+ list.innerHTML(populate);
+})
