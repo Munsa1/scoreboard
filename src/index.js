b98c0cdd2daf1fcd6e9e161b034553da16895f3a
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-11-12 17:03:35 +0200

    Create load scores function in index.js and use the pullData function to get the scorelist fro the api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import { initUsers, pullData, newGame } from './api';
 import './style.css';
 import { scorelog } from './scores.js';
 
+const loadScores = async () => {
+  const displayScores = document.getElementById('scorelist');
+
+  while (displayScores.firstChild) {
+    displayScores.removeChild(displayScores.firstChild);
+  }
+
+  const userData = await pullData();
+
+  userData.result.forEach((entry) => displayScores.insertAdjacentHTML('beforeend', `
+    <div>${entry.user}: ${entry.score}</div>  
+  `));
+};
+
+
+
+
 const myList = [
   {
     name: 'Munsa Mibenge',
