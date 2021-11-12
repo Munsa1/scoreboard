f95191ffbc34d034f854d1f8ffb5c0412ab6d312
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-11-12 16:31:45 +0200

    Declare API link and API endpoint

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const apiLink = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
-
+const apiEndPoint = 'DanielsNewApi';
 
 const newGame = async (name) => {
   const getResponse = await fetch(apiLink, {
